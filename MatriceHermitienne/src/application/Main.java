210d3f2aaa9171de0f62fcc116422f5d78a60dd1
Magiik32 <florentmamprin@gmail.com>
2018-10-16 23:19:43 +0200

    Synchronisation de l'ancienne version

--- MatriceHermitienne/src/application/Main.java
+++ MatriceHermitienne/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -10,12 +11,21 @@ import javafx.fxml.FXMLLoader;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+		
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("MatriceHermitienne.fxml"));
-			Scene scene = new Scene(root,600,600);
+			Scene scene = new Scene(root,850,900);
+			
+			// Titre de la fenêtre
+			primaryStage.setTitle("Valeurs propres d'une Matrice Hermitienne");
+			
+			// Logo 3iL
+			primaryStage.getIcons().setAll(new Image(getClass().getResource("3ilogo.png").toExternalForm()));
+			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- MatriceHermitienne/src/application/MatriceHermitienneController.java
+++ MatriceHermitienne/src/application/MatriceHermitienneController.java
@@ -1,5 +1,23 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MatriceHermitienneController {
+	
+	@FXML
+	private Button BtTest;
+	
+	@FXML
+	public void actionBtTest(ActionEvent evt){
+
+		System.out.println("\nouai");
+	}
+	
+	@FXML
+	private void initialize(){
+		System.out.println("En début");
+	}
 	
 }
