f0902f55ac58f3cb0aa613e6cd60b03005221ac7
FlorentM <florentmamprin@gmail.com>
2018-10-17 21:45:57 +0200

    Mise en place des boutons radio

--- MatriceHermitienne/src/application/MatriceHermitienneController.java
+++ MatriceHermitienne/src/application/MatriceHermitienneController.java
@@ -3,21 +3,12 @@ package application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 
 public class MatriceHermitienneController {
 	
-	@FXML
-	private Button BtTest;
-	
-	@FXML
-	public void actionBtTest(ActionEvent evt){
 
-		System.out.println("\nouai");
-	}
+		
 	
-	@FXML
-	private void initialize(){
-		System.out.println("En début");
-	}
 	
 }
