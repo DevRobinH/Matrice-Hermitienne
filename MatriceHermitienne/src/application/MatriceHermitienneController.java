53be597559481e0be4090f201f4b809db3576f6d
FlorentM <florentmamprin@gmail.com>
2018-10-20 13:00:33 +0200

    Changement du Logo. Mise en place de la Matrice. Pas encore codée pour les cases disabled, en cours.

--- MatriceHermitienne/src/application/MatriceHermitienneController.java
+++ MatriceHermitienne/src/application/MatriceHermitienneController.java
@@ -4,11 +4,17 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 
 public class MatriceHermitienneController {
 	
-
-		
+	@FXML
+	private TextField champMatrice1_1, champMatrice1_2, champMatrice1_3;
 	
+	@FXML
+	private TextField champMatrice2_1, champMatrice2_2,champMatrice2_3;
+	
+	@FXML
+	private TextField champMatrice3_1,champMatrice3_2,champMatrice3_3;
 	
 }
